a61a82a605d7ccafb17de327cbf2ab8223113914
Roberta Pennisi <roberta.pennisi97@gmail.com>
2021-10-20 12:04:22 +0200

    prove js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-let arr = [];
+//             0     1        2         3       4
+const arr = ["Dog","Cat","Goldfish","Monkey","Panda"];
 
+// questo aggiunge elementi alla coda dell'array
 arr.push('Ignazio','Marco','Stefania')
+
+//splice(start, count, el, el, ...) 
+//in questo caso 2 elementi vengono tolti dalla posizione 2 e vengono sostituiti dai valori a seguire
+let result = arr.splice(2, 2, 'Ignazio', 434, ['altro', 'array']);
+
+//splice(start, count, el, el, ...) 
+//in questo caso vengono estratti 2 elementi dalla posizione 2
+let result = arr.splice(2, 2);
+
+
+console.log(arr);
+console.log(result);
---
+++ js/app1.js
@@ -0,0 +1,7 @@
+let arr1 = [];
+arr1.push('Ignazio','Marco','Stefania');
+
+let arr2 = [1,2,3,4,5];
+let arr3 = ['fffff' , 'ggggg'];
+
+console.log(arr1.concat(arr2, arr3));
